174a4c0a99e71e52c8d060d6bdd73d2a8efe71e1
srxrt <str9703@gmail.com>
2025-04-21 02:48:26 +0900

    feat: add kulala and indentscope

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,53 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        sync_install=false,
-        highlight = { enable = true },
-        indent = { enable = true }, 
-        ensure_installed = {
-          "bash", 
-          "c",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "lua",
-          "luadoc",
-          "luap", "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "toml",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml",
-          "go",
-          "gomod",
-          "gowork",
-          "gosum",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            node_incremental = "<Enter>",
-            scope_incremental = false,
-            init_selection = "<Enter>",
-            node_decremental = "<Backspace>",
-          },
-        }
-      })
-    end
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+					"go",
+					"gomod",
+					"gowork",
+					"gosum",
+					"http",
+					"graphql",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						node_incremental = "<Enter>",
+						scope_incremental = false,
+						init_selection = "<Enter>",
+						node_decremental = "<Backspace>",
+					},
+				},
+			})
+		end,
+	},
 }
